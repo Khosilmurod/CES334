0fdcbc12674597949e0e751add6bb0c7d5181977
Murad Abdukholikov <m.abdukholikov@yale.edu>
2025-09-04 16:11:22 -0400

    updated config files and ip

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 echo "starting script"
-cp *config.txt /student334/CES334/raspberrypi/configs/
+cp /boot/firmware/config.txt /student334/CES334/raspberrypi/configs/
 hostname -I | awk '{print $1}' > ~/CES334/raspberrypi/configs/ip.md
 git add .
 git commit -m"updated config files and ip"
