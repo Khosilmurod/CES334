28481565ccf73c1c5c54966bde55b339925318f3
Murad Abdukholikov <m.abdukholikov@yale.edu>
2025-09-04 16:12:37 -0400

    updated config files and ip

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 echo "starting script"
-cp /boot/firmware/config.txt ./config/
+cp /boot/firmware/config.txt ./config/config.txt
 hostname -I | awk '{print $1}' > ./ip.md
 git add .
 git commit -m"updated config files and ip"
